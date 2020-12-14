1b119eeaddbf8b900661143e291c3c806fd87989
Joshua Vernon <josverno@amazon.com>
2020-12-13 17:23:50 -0800

    Added CONTRIBUTING.md + Updated jest coverage

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
   coverageReporters: ['json', 'json-summary', 'text', 'lcov'],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
+      branches: 99,
+      functions: 99,
+      lines: 99,
+      statements: 99
     }
   },
   globals: {
